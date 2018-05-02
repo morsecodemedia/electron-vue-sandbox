710025df487c745bcc73fee5c3af8774cc895be5
Brandon Morse <brandon@morsecodemedia.com>
2018-05-02 12:26:51 -0400

    stubbing out new screensaver compoenent

---
+++ src/renderer/components/Screensaver.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="wrapper">
+    <main>
+
+    </main>
+  </div>
+</template>
+
+<script>
+//import { TweenLite } from 'gsap'
+const fs = require('fs')
+
+const testFolder = process.env.PORTABLE_EXECUTABLE_DIR + '/test/'
+
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    window.console(file)
+  })
+})
+
+export default {
+  name: 'Screensaver',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style>
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+</style>
+
