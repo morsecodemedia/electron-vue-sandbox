d7be4d2d803fa95a04b2ead66bf7a22334526a42
Brandon Morse <brandon@morsecodemedia.com>
2018-05-02 15:52:53 -0400

    completed base requirements for screensaver component

--- src/renderer/components/Screensaver.vue
+++ src/renderer/components/Screensaver.vue
@@ -1,31 +1,59 @@
 <template>
   <div id="wrapper">
     <main>
-
+      <picture
+        v-for="picture in pictures"
+        :key="picture.id">
+        <source :srcset="picture.href">
+        <img
+          :src="picture.href"
+          :id="picture.id" />
+      </picture>
     </main>
   </div>
 </template>
 
 <script>
-//import { TweenLite } from 'gsap'
-const fs = require('fs')
+  import { TimelineMax } from 'gsap'
+  import path from 'path'
+  import { remote } from 'electron'
+  const fs = require('fs')
+  const testFolder = path.join(remote.app.getPath('desktop'), 'screensaver/')
 
-const testFolder = process.env.PORTABLE_EXECUTABLE_DIR + '/test/'
+  var tl = new TimelineMax({repeat:-1})
 
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    window.console(file)
-  })
-})
+  var fadeConfig = {
+    fadeSpeed: 4,
+    displayLength: 4
+  }
 
-export default {
-  name: 'Screensaver',
-  data() {
-    return {
-
-    }
-  },
-}
+  export default {
+    name: 'Screensaver',
+    data: function() {
+      return {
+        pictures: [],
+      }
+    },
+    updated () {
+      tl.clear()
+      this.pictures.forEach((picture) => {
+        let el = document.querySelector('#'+picture.id)
+        tl.to(el, fadeConfig.fadeSpeed, {alpha: 1}, "-="+fadeConfig.fadeSpeed)
+        tl.to('#'+picture.id, fadeConfig.displayLength, {alpha: 0}, "+="+(fadeConfig.fadeSpeed+fadeConfig.displayLength))
+      })
+      tl.play()
+    },
+    beforeMount () {
+      fs.readdir(testFolder, (err, files) => {
+        files.forEach((file,i) => {
+          this.pictures.push({
+            href: 'file://'+testFolder+file,
+            id: 'picture-'+i,
+            })
+        })
+      })
+    },
+  }
 </script>
 
 <style>
@@ -34,6 +62,18 @@ export default {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    background: black;
+  }
+
+  img {
+    object-fit: contain;
+    display: block;
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
   }
 
 </style>
