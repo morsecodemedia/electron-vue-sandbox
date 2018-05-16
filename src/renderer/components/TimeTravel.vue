7f5965b6a8c22d36694b8c869b67806be6b6a579
Brandon Morse <brandon@morsecodemedia.com>
2018-05-16 17:56:23 -0400

    began working on the timeTravel component. This will allow the user to select from a list of prefabbed destinationed (from a config file) to get the current travel time from the location of their smart mirror (ie. their home to work). Only commiting an emptied config file so api keys and other personal data don't make their way into the repo.

---
+++ src/renderer/components/TimeTravel.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <div class="btn"
+        v-for="loc in locations"
+        :key="loc.id">
+        <p>{{loc.label}}</p>
+        <address>
+          <span>{{loc.address.address1}}</span><br />
+          <span v-if="loc.address.address2">{{loc.address.address2}}<br /></span>
+          <span v-if="loc.address.address3">{{loc.address.address3}}<br /></span>
+          <span>{{loc.address.city}}, {{loc.address.state}} {{loc.address.zip}}</span>
+        </address>
+        <p v-if="loc.latitude && loc.longitude">{{loc.latitude}}, {{loc.longitude}}</p>
+      </div>
+      <p class="home-location">Your location is {{home.address.address1}}, <span v-if="home.address.address2">{{home.address.address2}}</span>
+        <span v-if="home.address.address3">{{home.address.address3}}</span> {{home.address.city}}, {{home.address.state}} {{home.address.zip}}. <span v-if="home.latitude && home.longitude">{{home.latitude}}, {{home.longitude}}</span></p>
+    </main>
+  </div>
+</template>
+
+<script>
+  import timeTravelConfig from './timeTravelConfig.json'
+  export default {
+    name: 'TimeTravel',
+    data: function() {
+      return {
+        locations: timeTravelConfig.locations,
+        home: timeTravelConfig.home
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  .btn {
+    display: inline-block;
+    background-color: white;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px 15px;
+    margin: 10px;
+    cursor: pointer;
+  }
+
+  .home-location {
+    position: absolute;
+    bottom: 15px;
+    text-align: center;
+    display: block;
+    width: 100vw;
+  }
+</style>
+
